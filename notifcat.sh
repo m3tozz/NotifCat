438944a7127b7d14f6ac25f825e4eb8ab52ef252
metozz <79897762+m3tozz@users.noreply.github.com>
2023-05-05 11:13:59 +0300

    Version 1.2.1!

--- notifcat.sh
+++ notifcat.sh
@@ -3,6 +3,7 @@ clear
 case $1 in
 
 esac
+[ $UID != 0 ] && { echo -e "\e[1;34m→\e[1;33mPlease run as '\e[1;31msudo bash ./notifcat.sh\e[1;33m'" ; exit 1 ; }
 banner(){
 echo -e '\e[31m
 ⠀⠀⠀⢠⣾⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -14,18 +15,18 @@ echo -e '\e[31m
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀          _   _       _   _  __  _____      _ 1.2 
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀          _   _       _   _  __  _____      _ 1.2.1
 ⣿⣿⣿⡇⠀⡾⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀         | \ | |     | | (_)/ _|/ ____|    | |  
 ⣿⣿⣿⣧⡀⠁⣀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀         |  \| | ___ | |_ _| |_| |     __ _| |_ 
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠉⢹⠉⠙⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀         | . ` |/ _ \| __| |  _| |    / _` | __|
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣀⠀⣀⣼⣿⣿⣿⣿   ⠀⠀⠀⠀           | |\  | (_) | |_| | | | |___| (_| | |_ 
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣀⠀⣀⣼⣿⣿⣿⣿   ⠀⠀⠀⠀          | |\  | (_) | |_| | | | |___| (_| | |_ 
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀         |_| \_|\___/ \__|_|_|  \_____\__,_|\__|
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀         XFCE Notification Theme Pack.
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠀⠤⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⠿⠋⢃⠈⠢⡁⠒⠄⡀⠈⠁⠀⠀⠀⠀⠀⠀⠀
 ⣿⣿⠟⠁⠀⠀⠈⠉⠉⠁⠀⠀⠀⠀⠈⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                       
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                       
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                       
 '
 echo -e '
 \e[1;34m[01]\e[0;32mInstall \e[1;35m[02]\e[0;32mRemove\e[1;34m \e[1;31m[x]\e[0;32mExit
@@ -48,7 +49,7 @@ if [[ $islem == 1 || $islem == 01 ]]; then
  |_| \_|\___/ \__|_|_|  \_____\__,_|\__|⠀⠀⠀⠀⠀                                       
 '
 
-cd theme/ && cp -r boardes dptzz firemeto hotvibet matrixz pinkmtz rocksur starless bluex metores aleyn forcepink silvetin silvetines galaxye softie fakeblack forcered greenland icemeto notifas linepink purpless linered linegold linegreen linerine bluewhites lineblue /home/$USER/.themes/
+cd theme/ && cp -r boardes dptzz firemeto hotvibet matrixz pinkmtz rocksur starless bluex metores aleyn forcepink silvetin silvetines galaxye softie fakeblack forcered greenland icemeto notifas linepink purpless linered linegold linegreen linerine bluewhites lineblue /usr/share/themes/
 
 	sleep 1
 	sleep 1
@@ -68,35 +69,35 @@ elif [[ $islem == 2 || $islem == 02 ]]; then
  | |\  | (_) | |_| | | | |___| (_| | |_ 
  |_| \_|\___/ \__|_|_|  \_____\__,_|\__|⠀⠀⠀⠀⠀                                       
 '
-rm -r /home/$USER/.themes/fakeblack
-rm -r /home/$USER/.themes/bluex
-rm -r /home/$USER/.themes/forcered
-rm -r /home/$USER/.themes/greenland
-rm -r /home/$USER/.themes/icemeto
-rm -r /home/$USER/.themes/notifas
-rm -r /home/$USER/.themes/purpless
-rm -r /home/$USER/.themes/linered
-rm -r /home/$USER/.themes/linepink
-rm -r /home/$USER/.themes/linegold
-rm -r /home/$USER/.themes/linegreen
-rm -r /home/$USER/.themes/lineblue
-rm -r /home/$USER/.themes/linerine
-rm -r /home/$USER/.themes/bluewhites
-rm -r /home/$USER/.themes/galaxye
-rm -r /home/$USER/.themes/softie
-rm -r /home/$USER/.themes/silvetines
-rm -r /home/$USER/.themes/silvetin
-rm -r /home/$USER/.themes/metores
-rm -r /home/$USER/.themes/forcepink
-rm -r /home/$USER/.themes/aleyn
-rm -r /home/$USER/.themes/boardes
-rm -r /home/$USER/.themes/dptzz
-rm -r /home/$USER/.themes/firemeto
-rm -r /home/$USER/.themes/hotvibet
-rm -r /home/$USER/.themes/matrixz
-rm -r /home/$USER/.themes/pinkmtz
-rm -r /home/$USER/.themes/rocksur
-rm -r /home/$USER/.themes/starless
+rm -r /usr/share/themes/fakeblack
+rm -r /usr/share/themes/bluex
+rm -r /usr/share/themes/forcered
+rm -r /usr/share/themes/greenland
+rm -r /usr/share/themes/icemeto
+rm -r /usr/share/themes/notifas
+rm -r /usr/share/themes/purpless
+rm -r /usr/share/themes/linered
+rm -r /usr/share/themes/linepink
+rm -r /usr/share/themes/linegold
+rm -r /usr/share/themes/linegreen
+rm -r /usr/share/themes/lineblue
+rm -r /usr/share/themes/linerine
+rm -r /usr/share/themes/bluewhites
+rm -r /usr/share/themes/galaxye
+rm -r /usr/share/themes/softie
+rm -r /usr/share/themes/silvetines
+rm -r /usr/share/themes/silvetin
+rm -r /usr/share/themes/metores
+rm -r /usr/share/themes/forcepink
+rm -r /usr/share/themes/aleyn
+rm -r /usr/share/themes/boardes
+rm -r /usr/share/themes/dptzz
+rm -r /usr/share/themes/firemeto
+rm -r /usr/share/themes/hotvibet
+rm -r /usr/share/themes/matrixz
+rm -r /usr/share/themes/pinkmtz
+rm -r /usr/share/themes/rocksur
+rm -r /usr/share/themes/starless
 	sleep 1
 	sleep 1
 clear	
