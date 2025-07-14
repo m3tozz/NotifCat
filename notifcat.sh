08b3a55efe091c79f466b854fec4ce42219129d9
Metin Zühre <79897762+m3tozz@users.noreply.github.com>
2025-07-14 15:23:28 +0300

    Update notifcat.sh

--- notifcat.sh
+++ notifcat.sh
@@ -149,8 +149,8 @@ fi
  XFCE Notification Theme Pack!'
     echo -e "\e[1;34m##########################################################\033[0m"
     echo -e "    Create by           ":" M3TOZZ"
-    echo -e "    Github              ":" https://github.com/m3tozz"
-    echo -e "    Instagram           ":" @textzuhree"
+    echo -e "    Github              ":" github.com/m3tozz/NotifCat"
+    echo -e "    Community Server    ":" discord.com/invite/sQwYCZer95"
     echo -e "    Version             ":" NotifCat-1.2.2"
     echo -e "\e[1;34m##########################################################\033[0m"
 	exit 1
